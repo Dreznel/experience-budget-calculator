5ae09771488fd679b2fe8941d243c684b8120948
JohnCarloSalter <JohnCarloSalter@gmail.com>
2018-12-12 14:23:44 -0500

    Completely replicated my Google Sheets functionality. Unfortunately, I discovered bug in my actual process, but that's unrelated to this.

--- app.R
+++ app.R
@@ -7,20 +7,76 @@ ui <- fluidPage(
   headerPanel("Experience Budget"),
   
   sidebarPanel(
-    numericInput("num_characters", "Number of Characters", 4, step = 1),
-    numericInput("party_level", "Party Level", 1, step = 1)
+    numericInput("num_characters", "Number of Characters", 4, min = 1, step = 1),
+    numericInput("party_level", "Party Level", 1, min = 1, max = 20, step = 1)
   ),
   
   mainPanel(
-    tableOutput("budget_table")
+    tableOutput("budget_table"),
+    tableOutput("threshold_table")
   )
 )
 
 server <- function(input, output) {
   xp_thresholds <- read.csv("xp_thresholds.csv")
   xp_thresholds_table <- xtable(xp_thresholds, label = "XP Thresholds By Level")
-  output$budget_table <- renderTable(
+  output$threshold_table <- renderTable(
     xp_thresholds_table,
+    striped = TRUE,
+    bordered = TRUE
+  )
+  
+  
+  easy_budget <- reactiveVal()
+  easy_column <- reactiveVal() #c(xp_thresholds[input$party_level] * input$num_characters) )
+  medium_column <- reactiveVal()
+  hard_column <- reactiveVal()
+  deadly_column <- reactiveVal()
+  
+  
+  output$budget_table <- renderTable(
+    data.frame(
+      number_of_monsters = c(
+        "1",
+        "2",
+        "3-6",
+        "7-10",
+        "11-14",
+        "15+"
+      ),
+      easy = c(
+        xp_thresholds$easy[input$party_level] * input$num_characters / get_encounter_multiplier(1),
+        xp_thresholds$easy[input$party_level] * input$num_characters / get_encounter_multiplier(2),
+        xp_thresholds$easy[input$party_level] * input$num_characters / get_encounter_multiplier(3),
+        xp_thresholds$easy[input$party_level] * input$num_characters / get_encounter_multiplier(7),
+        xp_thresholds$easy[input$party_level] * input$num_characters / get_encounter_multiplier(11),
+        xp_thresholds$easy[input$party_level] * input$num_characters / get_encounter_multiplier(15)
+      ),
+      medium = c(
+        xp_thresholds$medium[input$party_level] * input$num_characters / get_encounter_multiplier(1),
+        xp_thresholds$medium[input$party_level] * input$num_characters / get_encounter_multiplier(2),
+        xp_thresholds$medium[input$party_level] * input$num_characters / get_encounter_multiplier(3),
+        xp_thresholds$medium[input$party_level] * input$num_characters / get_encounter_multiplier(7),
+        xp_thresholds$medium[input$party_level] * input$num_characters / get_encounter_multiplier(11),
+        xp_thresholds$medium[input$party_level] * input$num_characters / get_encounter_multiplier(15)
+      ),
+      hard = c(
+        xp_thresholds$hard[input$party_level] * input$num_characters / get_encounter_multiplier(1),
+        xp_thresholds$hard[input$party_level] * input$num_characters / get_encounter_multiplier(2),
+        xp_thresholds$hard[input$party_level] * input$num_characters / get_encounter_multiplier(3),
+        xp_thresholds$hard[input$party_level] * input$num_characters / get_encounter_multiplier(7),
+        xp_thresholds$hard[input$party_level] * input$num_characters / get_encounter_multiplier(11),
+        xp_thresholds$hard[input$party_level] * input$num_characters / get_encounter_multiplier(15)
+      ),
+      deadly = c(
+        xp_thresholds$deadly[input$party_level] * input$num_characters / get_encounter_multiplier(1),
+        xp_thresholds$deadly[input$party_level] * input$num_characters / get_encounter_multiplier(2),
+        xp_thresholds$deadly[input$party_level] * input$num_characters / get_encounter_multiplier(3),
+        xp_thresholds$deadly[input$party_level] * input$num_characters / get_encounter_multiplier(7),
+        xp_thresholds$deadly[input$party_level] * input$num_characters / get_encounter_multiplier(11),
+        xp_thresholds$deadly[input$party_level] * input$num_characters / get_encounter_multiplier(15)
+      )
+    ),
     striped = TRUE,
     bordered = TRUE
   )
